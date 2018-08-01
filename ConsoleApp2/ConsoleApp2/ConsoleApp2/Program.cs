f654edcea22e46aefd01704262010e1b23f55a7b
Janos, Palvolgyi <30524592+Johnsee63@users.noreply.github.com>
2018-08-01 21:20:03 +0200

    Friss anyagok
    A mai termés

---
+++ ConsoleApp2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class MyClass
+    {
+        public int x;   //publikus változó deklarálás az osztályon belül.
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass s = new MyClass();
+            s.x = 10;
+
+            MyClass p = s;
+            p.x = 14;
+
+            Console.WriteLine(s.x);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = "Dinamikus típus használata";
+            Console.WriteLine(title);
+            Console.WriteLine();
+
+            dynamic x = 10;
+            Console.WriteLine("X -> " + x);   // x értéke most 10.
+            Console.WriteLine();
+
+            x = "Stifolder";
+            Console.WriteLine("X -> "+x);   // x értéke most szalámi.
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
