0ee9bd4206989ea7e6c5341c883e9e60af24f7e1
Janos, Palvolgyi <30524592+Johnsee63@users.noreply.github.com>
2018-08-07 20:45:42 +0200

    A mai termés

---
+++ Guess/Guess/Guess/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Szám kitalálós játék a számítógéppel");
+            Console.WriteLine();
+
+            Random something = new Random();
+
+            int cNumber = 0;
+            int pNumber = 0;
+            int i = 1;
+            int min = 1;
+            int max = 100;
+
+        START:
+            Console.WriteLine("Mindenkinek öt tippje van. Válassz játékmódot!");
+            Console.WriteLine("1 - Te gondolsz egy számra.");
+            Console.WriteLine("2 - A számítógép gondol egy számra.");
+            Console.WriteLine();
+
+            switch (Console.ReadKey(true).KeyChar)
+            {
+                case '1': goto PLAYER;
+                case '2': goto COMPUTER;
+            }
+
+        PLAYER:
+            cNumber = 50;
+
+            Console.WriteLine("Gondolj egy egész számra 1 és 100 között.");
+            Console.WriteLine();
+
+            i = 1;
+
+            while (i < 5)
+            {
+                Console.WriteLine("A számítógép szerint a szám {0}.", cNumber);
+                Console.WriteLine("Segíts egy kicsit, szerinted (k - kisebb, n - nagyobb, e - egyenlő)?");
+
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case 'k':
+                        if (i == 3)
+                            cNumber = something.Next(min, cNumber);
+                        else
+                        {
+                            max = cNumber;
+                            cNumber -= (max - min) / 2;
+                        }
+                        break;
+                    case 'n':
+                        if (i == 3)
+                            cNumber = something.Next(cNumber + 1, max);
+                        else
+                        {
+                            min = cNumber;
+                            cNumber += (max - min) / 2;
+                        }
+                        break;
+                    case 'e':
+                        Console.WriteLine("A számítógép nyert!");
+                        goto END;
+                }
+            ++i;
+            }
+            Console.WriteLine("Gratulálok, nyertél! A számítógép nem tudta kitalálni a számot.");
+            goto END;
+
+        COMPUTER:
+            cNumber = something.Next(min, max);
+
+            Console.WriteLine("A számítógép gondolt egy számra 1 és 100 között.");
+
+            while (i < 5)
+            {
+                Console.WriteLine("\nAz " + i + ". tipped:");
+                pNumber = int.Parse(Console.ReadLine());
+
+                if (cNumber > pNumber)
+                {
+                    Console.WriteLine("A szám ennél nagyobb!");
+                }
+                else if (cNumber < pNumber)
+                {
+                    Console.WriteLine("A szám ennél kissebb!");
+                }
+                else
+                {
+                    Console.WriteLine("Gratulálok, nyertél!");
+                    goto END;
+                }
+                ++i;
+            }
+
+            Console.WriteLine("\nSajnálom, vesztettél! A szám {0} volt.", cNumber);
+            goto END;
+
+        END:
+            Console.WriteLine("\nAkarsz még Játszani (i/n)?");
+
+            switch (Console.ReadKey(true).KeyChar)
+            {
+                case 'i': goto START;
+                case 'n': break;
+                default: goto END;
+            }
+        }
+    }
+}
---
+++ StonePaperScissors/StonePaperScissors/StonePaperScissors/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StonePaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random something = new Random();
+
+            string cChoice = "";
+            string pChoice = "";
+            int cScore = 0;
+            int pScore = 0;
+
+            bool sequel = true;
+
+            Console.WriteLine("Kő - Papír - Olló játék a számítógéppel");
+            Console.WriteLine();
+
+            do
+            {
+                Console.WriteLine("Mit választasz (k/p/o)? ");  // A játékos választ.
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case 'k':
+                        pChoice = "kő";
+                        break;
+
+                    case 'p':
+                        pChoice = "papír";
+                        break;
+
+                    case 'o':
+                        pChoice = "olló";
+                        break;
+                }
+
+                switch (something.Next(0, 2))   // A számítógép választ.
+                {
+                    case 0:
+                        cChoice = "kő";
+                        break;
+
+                    case 1:
+                        cChoice = "papír";
+                        break;
+
+                    case 2:
+                        cChoice = "olló";
+                        break;
+                }
+
+                Console.WriteLine("Játékos választása: " + pChoice + " A számítógép választása: " + cChoice);
+
+                if (
+                    (pChoice=="kő" && cChoice=="papír")
+                    ||
+                    (pChoice=="papír" && cChoice=="olló")
+                    ||
+                    (pChoice=="olló" && cChoice=="kő")
+                    )   // A kapott válaszok kiértékelése.
+                {
+                    Console.WriteLine("Sajnálom, veszítettél! Az állás \nSzámítógép: {0} \nJátékos: {1}", ++cScore, pScore);
+                }
+                else if (pChoice==cChoice)
+                {
+                    Console.WriteLine("Megízzasztottál, döntetlen! Az állás \nSzámítógép: {0} \nJátékos: {1}", cScore, pScore);
+                }
+                else
+                {
+                    Console.WriteLine("Gratulálok, nyertél! Az állás \nSzámítógép: {0} \nJátékos: {1}", cScore, ++pScore);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Folytasuk (i/n)? ");
+                if (Console.ReadKey(true).KeyChar == 'n')
+                    sequel = false;
+            } while (sequel);
+        }
+    }
+}
