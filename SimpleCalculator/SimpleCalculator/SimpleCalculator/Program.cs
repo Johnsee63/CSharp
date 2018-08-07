a4679f3fbc805312e22bc3b81b52b64bed8039ac
Janos, Palvolgyi <30524592+Johnsee63@users.noreply.github.com>
2018-08-07 17:20:39 +0200

    A mai termés

---
+++ SimpleCalculator/SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stNumber=0, ndNumber=0, result=0;
+            char operandus='+';
+
+            Console.WriteLine("Egyszerű számológép");
+            Console.WriteLine();
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine(" Kérem az első számot: ");
+                stNumber = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Kérem a második számot: ");
+                ndNumber = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Mi legyen a művelet (+, -, *, /): ");
+                operandus = Convert.ToChar(Console.Read());
+
+                switch (operandus)
+                {
+                    case '+':
+                        result = stNumber + ndNumber;
+                        break;
+
+                    case '-':
+                        result = stNumber - ndNumber;
+                        break;
+
+                    case '*':
+                        result = stNumber * ndNumber;
+                        break;
+
+                    case '/':
+                        result = stNumber / ndNumber;
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("A művelet eredménye: {0}", result);
+            }
+            else
+            {
+                if (args.Length != 3)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Hiba!");
+                    Console.WriteLine("Nem megfelelő számú a megadott paraméter.");
+                }
+                else
+                {
+                    stNumber = int.Parse(args[0]);
+                    ndNumber = int.Parse(args[1]);
+                    operandus = Convert.ToChar(args[2]);
+
+                    switch (operandus)
+                    {
+                        case '+':
+                            result = stNumber + ndNumber;
+                            break;
+
+                        case '-':
+                            result = stNumber - ndNumber;
+                            break;
+
+                        case '*':
+                            result = stNumber * ndNumber;
+                            break;
+
+                        case '/':
+                            result = stNumber / ndNumber;
+                            break;
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("A művelet eredménye: {0}", result);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
